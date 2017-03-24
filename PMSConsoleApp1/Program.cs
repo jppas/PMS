50a4773c69eceea5c4b137dfb2d6951b5e60b6de
Peevee Jace G. Pascual <peevee12@yahoo.com>
2017-03-24 10:55:20 -0600

    Add project files.

---
+++ PMSConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PMSConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
